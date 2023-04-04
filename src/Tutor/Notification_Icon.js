8afc0f07a792c54b9e9649d481f8f89759021976
saad khalid <khalidxaad@gmail.com>
2023-04-04 17:43:33 +0500

    set counter for notifications

--- src/Tutor/Notification_Icon.js
+++ src/Tutor/Notification_Icon.js
@@ -1,10 +1,61 @@
 /* eslint-disable prettier/prettier */
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Notification_Icon({ navigation }) {
-    const [counter, setCounter] = useState(2);
+    const [counter, setCounter] = useState(0);
+    const [tEmail, setTEmail] = useState('');
+
+    useEffect(() => {
+        getgmail();
+    }, []);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if (tEmail !== '') {
+                get_notifications();
+            }
+        }, 2000);
+        return () => clearInterval(intervalId);
+    }, [tEmail]);
+
+
+    const getgmail = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('std_email');
+            if (jsonValue != null) {
+                setTEmail(jsonValue);
+                console.log('Getting the email address of tutor from Asyncstorage => ', jsonValue);
+                console.log('----------------------------------------------------------------------------');
+            } else {
+                console.log('No gmail found in Asyncstorage');
+                console.log('----------------------------------------------------------------------------');
+            }
+        } catch (e) {
+            console.log(e);
+            console.log('----------------------------------------------------------------------------');
+        }
+    };
+
+    const get_notifications = async () => {
+        try {
+            const response = await fetch(
+                `http://192.168.43.231/HouseOfTutors/api/Tutor/GetStudentRequests?temail=${tEmail}`,
+            );
+            const data = await response.json();
+            console.log('data from get notifications', data);
+            console.log('----------------------------------------------------------------------------');
+            if (data !== 'No Requests') {
+                setCounter(data.length);
+            }
+        } catch (error) {
+            console.log(error);
+            console.log('----------------------------------------------------------------------------');
+        }
+    };
+
     return (
         <View>
             <View style={style.main_container}>
