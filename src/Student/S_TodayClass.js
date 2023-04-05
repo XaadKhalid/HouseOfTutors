a64c17e4eb7deefb46fb62a78bca06b24ec548d8
saad khalid <khalidxaad@gmail.com>
2023-04-05 22:33:05 +0500

    screen updated with real time data using API

--- src/Student/S_TodayClass.js
+++ src/Student/S_TodayClass.js
@@ -1,95 +1,158 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+/* eslint-disable prettier/prettier */
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Alert, Pressable } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function S_TodayClass() {
+  const [todayClassesList, setTodayClassesList] = useState([]);
+  const [stdEmail, setStdEmail] = useState('');
+
+  useEffect(() => {
+    getgmail();
+  }, []);
+
+  useEffect(() => {
+    if (stdEmail !== '') {
+      console.log('Email address against which classes are fetched is ', stdEmail);
+      get_today_classes();
+    }
+  }, [stdEmail]);
+
+  const getgmail = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('std_email');
+      if (jsonValue != null) {
+        setStdEmail(jsonValue);
+        console.log(
+          'Getting the email address of student from Asyncstorage => ',
+          jsonValue,
+        );
+        console.log(
+          '----------------------------------------------------------------------------',
+        );
+      } else {
+        console.log('No gmail found in Asyncstorage');
+        console.log(
+          '----------------------------------------------------------------------------',
+        );
+      }
+    } catch (e) {
+      console.log(e);
+      console.log(
+        '----------------------------------------------------------------------------',
+      );
+    }
+  };
+
+  const get_today_classes = async () => {
+    try {
+      const response = await fetch(
+        `http://192.168.43.231/HouseOfTutors/api/Student/Get_Today_Classes?semail=${stdEmail}`,
+      );
+      const data = await response.json();
+      console.log('Result from get_today_classes API =>', data);
+      console.log(
+        '----------------------------------------------------------------------------',
+      );
+      if (data !== 'No class are schedule for today' && data !== 'No Record Found in the Enrollment') {
+        setTodayClassesList(data);
+      } else {
+        Alert.alert('No class are schedule for today');
+        console.log(
+          '----------------------------------------------------------------------------',
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View style={styles.main_container}>
-      <View style={styles.header}>
-        <View>
-          <Text style={styles.header_txt}>Tutor</Text>
-        </View>
-        <View>
-          <Text style={styles.header_txt}>Course</Text>
-        </View>
-        <View>
-          <Text style={styles.header_txt}>Class Time</Text>
-        </View>
+    <View>
+      <View style={styles.heading}>
+        <Text style={styles.h_text}>CName</Text>
+        <Text style={styles.h_text}>Student</Text>
+        <Text style={styles.h_text}>Class</Text>
+        <Text style={styles.h_text}>Status</Text>
       </View>
-      <View style={styles.details_continer}>
-        <View>
-          <Text style={styles.details_txt}>Aftab</Text>
+      {!todayClassesList && (
+        <View style={styles.FList_BM}>
+          <FlatList
+            data={todayClassesList}
+            renderItem={({ item }) => (
+              <View style={styles.modal}>
+                {/* <Text style={styles.text}>CID: {item.cid}</Text> */}
+                <Text style={styles.text}>{item.ccode}</Text>
+                <Text style={styles.text}>{item.cname}</Text>
+                <View>
+                  <Pressable
+                    style={styles.btn}
+                    onPress={() => {
+                      console.log(
+                        'course id and email to be passed in enlist course api is ',
+                        item.cid,
+                        stdEmail,
+                      );
+                      console.log(
+                        '----------------------------------------------------------------------------',
+                      );
+                    }}>
+                    <Text style={styles.btn_text}>Take</Text>
+                  </Pressable>
+                </View>
+              </View>
+            )}
+          />
         </View>
-        <View>
-          <Text style={styles.details_txt}>OOP</Text>
-        </View>
-        <View>
-          <Text style={styles.details_txt}>08:30-09:30 AM</Text>
-        </View>
-      </View>
-      <View style={styles.details_continer}>
-        <View>
-          <Text style={styles.details_txt}>Umer</Text>
-        </View>
-        <View>
-          <Text style={styles.details_txt}>DSA</Text>
-        </View>
-        <View>
-          <Text style={styles.details_txt}>11:30-12:30 PM</Text>
-        </View>
-      </View>
-      <View style={styles.details_continer}>
-        <View>
-          <Text style={styles.details_txt}>Amir</Text>
-        </View>
-        <View>
-          <Text style={styles.details_txt}>DAM</Text>
-        </View>
-        <View>
-          <Text style={styles.details_txt}>03:30-04:30 PM</Text>
-        </View>
-      </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  main_container: {
-    padding: 10,
+  modal: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: 'rgba(102,24,231,0.7)',
+    marginHorizontal: 10,
+    paddingVertical: 15,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  text: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  heading: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
     backgroundColor: '#ffffff',
-    height: '100%',
+    marginHorizontal: 10,
+    paddingVertical: 15,
+    marginTop: 10,
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'rgba(102,24,231,0.9)',
-    padding: 22,
-    borderRadius: 10,
-  },
-  header_txt: {
-    color: '#ffffff',
+  h_text: {
+    color: '#000000',
     fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
   },
-  details_txt: {
-    color: '#ffffff',
-    fontWeight: 'bold',
-  },
-  details_continer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'rgba(102,24,231,0.8)',
-    padding: 18,
-    borderRadius: 10,
-    marginTop: 15,
-  },
-  Rate_btn: {
-    marginLeft: 'auto',
+  btn: {
+    backgroundColor: '#FFB22F',
     elevation: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    borderRadius: 5,
   },
-  Rate_btn_txt: {
-    backgroundColor: '#EB6440',
-    color: '#ffffff',
-    paddingHorizontal: 18,
-    paddingVertical: 3,
-    borderRadius: 8,
+  btn_text: {
+    color: '#000000',
+    fontWeight: '600',
+  },
+  FList_BM: {
+    marginBottom: 70,
   },
 });
--- src/Tutor/T_TodayClass.js
+++ src/Tutor/T_TodayClass.js
@@ -1,140 +1,189 @@
-import React from 'react';
-import { View, Text, StyleSheet, Pressable, Alert } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Alert, Pressable } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function S_TodayClass() {
+export default function T_TodayClass() {
+  const [todayClassesList, setTodayClassesList] = useState([]);
+  const [stdEmail, setStdEmail] = useState('');
+
+  useEffect(() => {
+    getgmail();
+  }, []);
+
+  useEffect(() => {
+    if (stdEmail !== '') {
+      get_today_classes();
+    }
+  }, [stdEmail]);
+
+  useEffect(() => {
+    if (todayClassesList.length > 0) {
+      console.log('updated list is ', todayClassesList);
+    }
+  }, [todayClassesList]);
+
+  const getgmail = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('std_email');
+      if (jsonValue != null) {
+        setStdEmail(jsonValue);
+        console.log(
+          'Getting the email address of student from Asyncstorage => ',
+          jsonValue,
+        );
+        console.log(
+          '----------------------------------------------------------------------------',
+        );
+      } else {
+        console.log('No gmail found in Asyncstorage');
+        console.log(
+          '----------------------------------------------------------------------------',
+        );
+      }
+    } catch (e) {
+      console.log(e);
+      console.log(
+        '----------------------------------------------------------------------------',
+      );
+    }
+  };
+
+  const get_today_classes = async () => {
+    try {
+      const response = await fetch(
+        `http://192.168.43.231/HouseOfTutors/api/Tutor/Get_Today_Classes?temail=${stdEmail}`,
+      );
+      const data = await response.json();
+      console.log('Result from get_today_classes API =>', data);
+      console.log(
+        '----------------------------------------------------------------------------',
+      );
+      if (
+        data !== 'No class are schedule for today' &&
+        data !== 'No Record Found in the Enrollment'
+      ) {
+        setTodayClassesList(data);
+      } else {
+        Alert.alert('No class are schedule for today');
+        console.log(
+          '----------------------------------------------------------------------------',
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.modal}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+        }}>
+        <Text style={styles.text}>{item.cname}</Text>
+        <Text style={styles.text}>{item.sname}</Text>
+        <Text style={styles.text}>{item.slotindexes.toString()}</Text>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginHorizontal: 42,
+        }}>
+        <Pressable
+          style={styles.btn}
+          onPress={() => {
+            console.log(
+              'Take is presseed!'
+            );
+            console.log(
+              '----------------------------------------------------------------------------',
+            );
+          }}>
+          <Text style={styles.btn_text}>Take</Text>
+        </Pressable>
+        <Pressable
+          style={styles.btn}
+          onPress={() => {
+            console.log(
+              'ReSchedule is presseed!'
+            );
+            console.log(
+              '----------------------------------------------------------------------------',
+            );
+          }}>
+          <Text style={styles.btn_text}>ReSchedule</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+
   return (
-    <View style={styles.main_container}>
-      <View style={styles.header}>
-        <View>
-          <Text style={styles.header_txt}>Student</Text>
-        </View>
-        <View>
-          <Text style={styles.header_txt}>Class Time</Text>
-        </View>
-        <View>
-          <Text style={styles.header_txt}>Status</Text>
-        </View>
+    <View>
+      <View style={styles.heading}>
+        <Text style={styles.h_text}>CName</Text>
+        <Text style={styles.h_text}>Student</Text>
+        <Text style={styles.h_text}>Time Slot</Text>
       </View>
-      <View style={styles.details_continer}>
-        <View>
-          <Text style={styles.details_txt}>Aftab</Text>
+      {todayClassesList && (
+        <View style={styles.FList_BM}>
+          <FlatList
+            data={todayClassesList}
+            renderItem={renderItem}
+          />
         </View>
-        <View>
-          <Text style={styles.details_txt}>08:30-09:30 AM</Text>
-        </View>
-        <View>
-          <Pressable
-            style={styles.Rate_btn}
-            onPress={() => {
-              Alert.alert('Tutor match request will be sent to API');
-            }}>
-            <Text style={styles.Rate_btn_txt}>Take</Text>
-          </Pressable>
-        </View>
-        <View>
-          <Pressable
-            style={styles.Rate_btn}
-            onPress={() => {
-              Alert.alert('Tutor match request will be sent to API');
-            }}>
-            <Text style={styles.Rate_btn_txt}>ReSchedule</Text>
-          </Pressable>
-        </View>
-      </View>
-      <View style={styles.details_continer}>
-        <View>
-          <Text style={styles.details_txt}>Aftab</Text>
-        </View>
-        <View>
-          <Text style={styles.details_txt}>08:30-09:30 AM</Text>
-        </View>
-        <View>
-          <Pressable
-            style={styles.Rate_btn}
-            onPress={() => {
-              Alert.alert('Tutor match request will be sent to API');
-            }}>
-            <Text style={styles.Rate_btn_txt}>Take</Text>
-          </Pressable>
-        </View>
-        <View>
-          <Pressable
-            style={styles.Rate_btn}
-            onPress={() => {
-              Alert.alert('Tutor match request will be sent to API');
-            }}>
-            <Text style={styles.Rate_btn_txt}>ReSchedule</Text>
-          </Pressable>
-        </View>
-      </View>
-      <View style={styles.details_continer}>
-        <View>
-          <Text style={styles.details_txt}>Aftab</Text>
-        </View>
-        <View>
-          <Text style={styles.details_txt}>08:30-09:30 AM</Text>
-        </View>
-        <View>
-          <Pressable
-            style={styles.Rate_btn}
-            onPress={() => {
-              Alert.alert('Tutor match request will be sent to API');
-            }}>
-            <Text style={styles.Rate_btn_txt}>Take</Text>
-          </Pressable>
-        </View>
-        <View>
-          <Pressable
-            style={styles.Rate_btn}
-            onPress={() => {
-              Alert.alert('Tutor match request will be sent to API');
-            }}>
-            <Text style={styles.Rate_btn_txt}>ReSchedule</Text>
-          </Pressable>
-        </View>
-      </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  main_container: {
-    padding: 10,
-    backgroundColor: '#ffffff',
-    height: '100%',
+  modal: {
+    justifyContent: 'center',
+    backgroundColor: 'rgba(102,24,231,0.7)',
+    marginHorizontal: 10,
+    paddingVertical: 15,
+    borderRadius: 5,
+    marginTop: 10,
+    borderColor: '#ffffff',
+    borderWidth: 2,
   },
-  header: {
+  text: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    marginBottom: 5,
+    //textAlign: 'center',
+  },
+  heading: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     backgroundColor: 'rgba(102,24,231,0.9)',
-    padding: 22,
-    borderRadius: 10,
+    marginHorizontal: 10,
+    paddingVertical: 15,
+    marginTop: 10,
   },
-  header_txt: {
+  h_text: {
     color: '#ffffff',
     fontWeight: 'bold',
+    marginHorizontal: 20,
+    textAlign: 'center',
   },
-  details_txt: {
-    color: '#ffffff',
-    fontWeight: 'bold',
-  },
-  details_continer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'rgba(102,24,231,0.8)',
-    padding: 18,
-    borderRadius: 10,
-    marginTop: 15,
-  },
-  Rate_btn: {
-    marginLeft: 'auto',
-    elevation: 10,
-  },
-  Rate_btn_txt: {
+  btn: {
     backgroundColor: '#FFB22F',
+    elevation: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  btn_text: {
     color: '#000000',
-    paddingHorizontal: 18,
-    paddingVertical: 3,
-    borderRadius: 8,
+    fontWeight: '600',
+  },
+  FList_BM: {
+    marginBottom: 70,
   },
 });
