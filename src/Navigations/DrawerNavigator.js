2ff9b84297b67a08fdde211b8f41c04444ac5007
XaadKhalid <khalidxaad@gmail.com>
2023-05-19 12:25:25 +0500

    headername title added for students

--- src/Navigations/DrawerNavigator.js
+++ src/Navigations/DrawerNavigator.js
@@ -1,13 +1,27 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import S_Bottom_Navigator from './S_Bottom_Navigator';
 import ReScheduleNotification from '../Screens/Student/ReScheduleNotification';
 import S_Profile from '../Screens/Student/S_Profile';
+import { getUserFormAsync } from '../AsyncStorage/GlobalData';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
+    const [name, setName] = useState(null);
+
+    useEffect(() => {
+        const getname = async () => {
+            let response = await getUserFormAsync();
+            if (response !== null) {
+                console.log('result is ', response.sname);
+                setName(response.sname);
+            }
+        };
+        getname();
+    }, []);
+
     return (
         <Drawer.Navigator screenOptions={{
             headerTitleAlign: 'center',
@@ -17,7 +31,7 @@ export default function DrawerNavigator() {
             },
         }}>
             <Drawer.Screen name="Home" component={S_Bottom_Navigator} options={{
-                headerTitle: 'Saad Khalid',
+                headerTitle: name,
             }} />
             <Drawer.Screen name="Schedule Requests" component={ReScheduleNotification} />
             <Drawer.Screen name="Profile" component={S_Profile} />
