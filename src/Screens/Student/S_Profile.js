02a01b3d2894ac87d17167d96be66869490a3283
XaadKhalid <khalidxaad@gmail.com>
2023-06-13 03:30:37 +0500

    Logout added to profile screens

--- src/Screens/Student/S_Profile.js
+++ src/Screens/Student/S_Profile.js
@@ -5,8 +5,10 @@ import { View, Text, Switch, Image, TouchableOpacity } from 'react-native';
 import styles from '../../Assests/Styling';
 import { getgmailFormAsync } from '../../AsyncStorage/GlobalData';
 import { GetWithParams } from '../../Api/API_Types';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 
-export default function S_Profile() {
+
+export default function S_Profile({ navigation }) {
 
   const [studentObj, setStudentObj] = useState({});
   const [isClass, setIsClass] = useState(false);
@@ -15,7 +17,7 @@ export default function S_Profile() {
   const handleChange = (setState) => (event) => {
     setState(event);
   };
-  const imagebg = require('../../Images/final_logo.png');
+  const imagebg = require('../../Images/sk.jpg');
 
   useEffect(() => {
     getProfile();
@@ -38,53 +40,65 @@ export default function S_Profile() {
 
   return (
     <View style={styles.bodyContainer}>
-      <View>
+      <View style={{ flex: 1 }}>
         <View>
           <TouchableOpacity onPress={() => { console.log('Image is pressed'); }}>
-            <Image source={imagebg} resizeMode="center" style={{ height: 150, width: 150 }} />
+            <Image source={imagebg} resizeMode="center" style={{ height: 150, width: 150, borderRadius: 150 / 2 }} />
           </TouchableOpacity>
           <Text style={{ marginTop: -74, marginLeft: 160 }}>{studentObj.sname}</Text>
           <Text style={{ marginTop: -4, marginLeft: 160, marginBottom: 35 }}>{studentObj.semail}</Text>
         </View>
         <View style={styles.containerbox}>
           <Text style={{ color: '#fff', fontWeight: 'bold', fontStyle: 'italic' }}>About Me :</Text>
-          <Text style={styles.itemText}>Teaching is a dynamic profession that nurtures minds and shapes futures. It involves imparting knowledge, fostering critical thinking, and igniting curiosity.</Text>
+          <Text style={styles.itemText}>Learning new things is an exhilarating journey of growth, curiosity, and exploration, expanding horizons, enriching knowledge, fostering creativity, and embracing the joy of discovery.</Text>
+        </View>
+        <View style={styles.containerbox}>
+          <Text style={styles.itemText}>Parents Can View?</Text>
+          <View style={styles.itembox}>
+            <Text style={styles.itemText}>Class Report</Text>
+            <Switch
+              trackColor={{ false: '#767577', true: '#81b0ff' }}
+              thumbColor={isClass ? '#f5dd4b' : '#f4f3f4'}
+              onValueChange={handleChange(setIsClass)}
+              value={isClass}
+            />
+          </View>
+          <View style={styles.itembox}>
+            <Text style={styles.itemText}>Fee Details</Text>
+            <Switch
+              trackColor={{ false: '#767577', true: '#81b0ff' }}
+              thumbColor={isFee ? '#f5dd4b' : '#f4f3f4'}
+              onValueChange={handleChange(setIsFee)}
+              value={isFee}
+            />
+          </View>
+          <View style={styles.itembox}>
+            <Text style={styles.itemText}>Attendance</Text>
+            <Switch
+              trackColor={{ false: '#767577', true: '#81b0ff' }}
+              thumbColor={isAttendance ? '#f5dd4b' : '#f4f3f4'}
+              onValueChange={handleChange(setIsAttendance)}
+              value={isAttendance}
+            />
+          </View>
+          <TouchableOpacity style={styles.button} onPressIn={() => {
+            console.log('Persmission button is pressed');
+          }}>
+            <Text style={styles.buttonText}>Save Permissions</Text>
+          </TouchableOpacity>
         </View>
       </View>
       <View style={styles.containerbox}>
-        <Text style={styles.itemText}>Parents Can View?</Text>
         <View style={styles.itembox}>
-          <Text style={styles.itemText}>Class Report</Text>
-          <Switch
-            trackColor={{ false: '#767577', true: '#81b0ff' }}
-            thumbColor={isClass ? '#f5dd4b' : '#f4f3f4'}
-            onValueChange={handleChange(setIsClass)}
-            value={isClass}
-          />
+          <Text style={[styles.itemText, { marginTop: 10 }]}>LogOut</Text>
+          <TouchableOpacity
+            style={styles.button2}
+            onPressIn={() => {
+              navigation.navigate('Login');
+            }}>
+            <SimpleLineIcons name={'logout'} size={20} color="#000000" />
+          </TouchableOpacity>
         </View>
-        <View style={styles.itembox}>
-          <Text style={styles.itemText}>Fee Details</Text>
-          <Switch
-            trackColor={{ false: '#767577', true: '#81b0ff' }}
-            thumbColor={isFee ? '#f5dd4b' : '#f4f3f4'}
-            onValueChange={handleChange(setIsFee)}
-            value={isFee}
-          />
-        </View>
-        <View style={styles.itembox}>
-          <Text style={styles.itemText}>Attendance</Text>
-          <Switch
-            trackColor={{ false: '#767577', true: '#81b0ff' }}
-            thumbColor={isAttendance ? '#f5dd4b' : '#f4f3f4'}
-            onValueChange={handleChange(setIsAttendance)}
-            value={isAttendance}
-          />
-        </View>
-        <TouchableOpacity style={styles.button} onPressIn={() => {
-          console.log('Persmission button is pressed');
-        }}>
-          <Text style={styles.buttonText}>Save Permissions</Text>
-        </TouchableOpacity>
       </View>
     </View>
   );
--- src/Screens/Tutor/T_Profile.js
+++ src/Screens/Tutor/T_Profile.js
@@ -5,11 +5,12 @@ import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from '../../Assests/Styling';
 import { getgmailFormAsync } from '../../AsyncStorage/GlobalData';
 import { GetWithParams } from '../../Api/API_Types';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 
-export default function T_Profile() {
+export default function T_Profile({ navigation }) {
 
   const [studentObj, setStudentObj] = useState({});
-  const imagebg = require('../../Images/final_logo.png');
+  const imagebg = require('../../Images/sk.jpg');
 
   useEffect(() => {
     getProfile();
@@ -32,10 +33,10 @@ export default function T_Profile() {
 
   return (
     <View style={styles.bodyContainer}>
-      <View>
+      <View style={{ flex: 1 }}>
         <View>
           <TouchableOpacity onPress={() => { console.log('Image is pressed'); }}>
-            <Image source={imagebg} resizeMode="center" style={{ height: 150, width: 150 }} />
+            <Image source={imagebg} resizeMode="center" style={{ height: 150, width: 150, borderRadius: 150 / 2 }} />
           </TouchableOpacity>
           <Text style={{ marginTop: -74, marginLeft: 160 }}>{studentObj.tname}</Text>
           <Text style={{ marginTop: -4, marginLeft: 160, marginBottom: 35 }}>{studentObj.temail}</Text>
@@ -44,28 +45,40 @@ export default function T_Profile() {
           <Text style={{ color: '#fff', fontWeight: 'bold', fontStyle: 'italic' }}>About Me :</Text>
           <Text style={styles.itemText}>Teaching is a dynamic profession that nurtures minds and shapes futures. It involves imparting knowledge, fostering critical thinking, and igniting curiosity.</Text>
         </View>
+        <View style={styles.containerbox}>
+          <Text style={styles.itemText}>Portfolio</Text>
+          <View style={styles.itembox}>
+            <Text style={styles.itemText}>Profile Rating</Text>
+            <Text style={styles.itemText}>*****</Text>
+          </View>
+          <View style={styles.itembox}>
+            <Text style={styles.itemText}>Total Earning</Text>
+            <Text style={styles.itemText}>*****</Text>
+          </View>
+          <View style={styles.itembox}>
+            <Text style={styles.itemText}>No of Students</Text>
+            <Text style={styles.itemText}>*****</Text>
+          </View>
+          <View style={styles.itembox}>
+            <Text style={styles.itemText}>No of Courses Taught</Text>
+            <Text style={styles.itemText}>*****</Text>
+          </View>
+          <View style={styles.itembox}>
+            <Text style={styles.itemText}>No of Lectures Delivered</Text>
+            <Text style={styles.itemText}>*****</Text>
+          </View>
+        </View>
       </View>
       <View style={styles.containerbox}>
-        <Text style={styles.itemText}>Portfolio</Text>
         <View style={styles.itembox}>
-          <Text style={styles.itemText}>Profile Rating</Text>
-          <Text style={styles.itemText}>*****</Text>
-        </View>
-        <View style={styles.itembox}>
-          <Text style={styles.itemText}>Total Earning</Text>
-          <Text style={styles.itemText}>*****</Text>
-        </View>
-        <View style={styles.itembox}>
-          <Text style={styles.itemText}>No of Students</Text>
-          <Text style={styles.itemText}>*****</Text>
-        </View>
-        <View style={styles.itembox}>
-          <Text style={styles.itemText}>No of Courses Taught</Text>
-          <Text style={styles.itemText}>*****</Text>
-        </View>
-        <View style={styles.itembox}>
-          <Text style={styles.itemText}>No of Lectures Delivered</Text>
-          <Text style={styles.itemText}>*****</Text>
+          <Text style={[styles.itemText, { marginTop: 10 }]}>LogOut</Text>
+          <TouchableOpacity
+            style={styles.button2}
+            onPressIn={() => {
+              navigation.navigate('Login');
+            }}>
+            <SimpleLineIcons name={'logout'} size={20} color="#000000" />
+          </TouchableOpacity>
         </View>
       </View>
     </View>
